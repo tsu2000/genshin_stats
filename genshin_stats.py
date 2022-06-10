80c260d46e811f5527c90d4bee9fa2d030b93ee6
tsu2000 <106811131+tsu2000@users.noreply.github.com>
2022-06-10 20:04:24 +0800

    Update genshin_stats.py

--- genshin_stats.py
+++ genshin_stats.py
@@ -182,7 +182,7 @@ def level_stats():
             st.markdown('**What do the values on the x-axis mean?**')
             st.markdown("- These are the levels a character is at. The number on the *left* refers to the **current level** the character is leveled to, while the number on the *right* refers to the **current level cap** of the character. The reason for this distinction is because a character's stats increases when they ascend despite not leveling up, (E.g. Stats at level 70 with cap at level 70 and cap at level 80 are different) and hence the need for such a distinction.")
             st.markdown('**Why do the number of characters sometimes not correspond with the number of lines shown on the graph?**')
-            st.markdown("- This is because some characters have the exact same stat progression as each other and one of the characters' lines is overlapping the others. (E.g. Amber & Kaeya) For greater clarity on this, it is highly recommended you check the data frame shown right below the plot to check which characters have the exact same stat progression. Furthermore, some characters may not have any data at all, so no such line may be drawn.")
+            st.markdown("- This is because some characters have the exact same stat progression as each other and one of the characters' lines is overlapping the others. (E.g. Amber & Kaeya) For greater clarity on this, it is highly recommended you check the data frame shown right below the plot to check which characters have the exact same stat progression. Furthermore, some characters may not have any statistical data at all, so it is not possible to draw a line for the character.")
 
         else:
             st.warning("You can only select a maximum of 12 characters!")
@@ -202,6 +202,60 @@ def spas_stats():
     st.markdown('### Ascension Stat Table')
     
     asc_stats = dc.loc[asc_char, ['ascension', 'ascension_specialty', 'ascension_talent_mat', 'ascension_boss_mat', 'special_0', 'special_1', 'special_2', 'special_3', 'special_4', 'special_5', 'special_6']].astype(str)
+    
+    if 'Hurricane Seed' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Anemo Hypostasis'
+        
+    if 'Lightning Prism' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Electro Hypostasis'
+
+    if 'Basalt Pillar' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Geo Hypostasis'
+        
+    if 'Hoarfrost Core' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Cryo Regisvine'
+        
+    if 'Everflame Seed' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Pyro Regisvine'
+        
+    if 'Cleansing Heart' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Rhodeia of Loch'
+
+    if 'Juvenile Jade' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Primo Geovishap'
+        
+    if 'Crystalline Bloom' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Cryo Hypostasis'
+        
+    if 'Marionette Core' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Maguu Kenki'
+        
+    if 'Perpetual Heart' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Perpetual Mechanical Array'
+
+    if 'Smoldering Pearl' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Pyro Hypostasis'
+        
+    if 'Dew of Repudiation' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Hydro Hypostasis'
+        
+    if 'Storm Beads' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Thunder Manifestation'
+        
+    if 'Riftborn Regalia' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Golden Wolflord'
+
+    if "Dragonheir's False Fin" in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Coral Defenders'
+        
+    if 'Runic Fang' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'Ruin Serpent'
+    
+    if 'nan' in asc_stats['ascension_boss_mat']:
+        asc_boss = 'nan'
+        
+    if 'Traveler' in asc_char:
+        asc_boss = 'None'
   
     fig = go.Figure(data = [go.Table(columnwidth = [4, 1.75],
                                      header = dict(values = [f'<b>Ascension Stats for {asc_char}<b>', '<b>Attributes<b>'],
@@ -217,7 +271,8 @@ def spas_stats():
                                                              'Base Special Ascension Stat at Ascension Level 5',
                                                              'Base Special Ascension Stat at Ascension Level 6',
                                                              'Ascension Regional Specialty',
-                                                             'Ascension Common Material',
+                                                             'Ascension Regular Material',
+                                                             'Ascension Boss',
                                                              'Ascension Boss Material'],
                                                             [asc_stats['ascension'],
                                                              asc_stats['special_0'],
@@ -229,6 +284,7 @@ def spas_stats():
                                                              asc_stats['special_6'],
                                                              asc_stats['ascension_specialty'],
                                                              asc_stats['ascension_talent_mat'], 
+                                                             asc_boss,
                                                              asc_stats['ascension_boss_mat']]], 
                                                   fill_color = 'lightcyan',
                                                   line_color = 'darkslategray',
@@ -240,6 +296,20 @@ def spas_stats():
 
     st.markdown('### View original source from csv file:')
     st.dataframe(asc_stats)
+    
+    st.markdown('### FAQ:')
+    st.markdown('**What do the Ascension Levels refer to?**')
+    st.markdown("- The Character Ascension Levels refers to the current level cap of the characters. In order to increase your level, you have to ascend, which means you will spend resources to increase your level cap to the next Ascension Level. The Ascension Levels and their corresponding character level caps are shown below:")
+    st.markdown('<div style="text-align: center;">Ascension Level 0: &emsp;<b>Lvl 20</b></div>', unsafe_allow_html = True)
+    st.markdown('<div style="text-align: center;">Ascension Level 1: &emsp;<b>Lvl 40</b></div>', unsafe_allow_html = True)
+    st.markdown('<div style="text-align: center;">Ascension Level 2: &emsp;<b>Lvl 50</b></div>', unsafe_allow_html = True)
+    st.markdown('<div style="text-align: center;">Ascension Level 3: &emsp;<b>Lvl 60</b></div>', unsafe_allow_html = True)
+    st.markdown('<div style="text-align: center;">Ascension Level 4: &emsp;<b>Lvl 70</b></div>', unsafe_allow_html = True)
+    st.markdown('<div style="text-align: center;">Ascension Level 5: &emsp;<b>Lvl 80</b></div>', unsafe_allow_html = True)
+    st.markdown('<div style="text-align: center;">Ascension Level 6: &emsp;<b>Lvl 90</b></div>', unsafe_allow_html = True)
+    st.markdown('###')
+    st.markdown("**What is a Special Ascencsion Stat?**")
+    st.markdown("- Every playable character in Genshin Impact is given a Special Ascension Stat, which is basically a boost to one of their particular stat upon reaching a certain Ascension Level. This boost to their stat increases as their ascension level increases. These stats can be considered separate from their Base Stats (HP, Attack, Defense) and thus have been categorised under the section of 'Character Ascension Information'.")
     
     st.markdown('---')
     
@@ -310,9 +380,9 @@ def talent_stats():
                                                              '3* Talent Material',
                                                              '4* Talent Material',
                                                              'Domain Location',
-                                                             'Talent Availability Days',
+                                                             'Days with Talent Availability*',
                                                              'Weekly Boss',
-                                                             'Weekly Boss Material'],
+                                                             'Weekly Boss Talent Material'],
                                                             [talent_stats[0],
                                                              talent_stats[1],
                                                              talent_stats[2],
@@ -325,13 +395,14 @@ def talent_stats():
                                                   align = ['left', 'center'],
                                                   font = dict(color = ['black', 'maroon'], size = [14, 14]),
                                                   height = 25))])
-    fig.update_layout(height = 250, width = 700, margin = dict(l = 5, r = 5, t = 5, b = 5))
+    fig.update_layout(height = 225, width = 700, margin = dict(l = 5, r = 5, t = 5, b = 5))
     st.write(fig)
+    st.markdown('*All Talent Books are available at all domains on Sunday.')
         
     st.markdown('### View original source from csv file:')
     st.dataframe(talent_stats)
     
-    st.markdown('All Talent Books are available at all domains on Sunday.')
+    
     
     st.markdown('---')
     
@@ -343,8 +414,8 @@ def trivia_stats():
     
     st.markdown('---')
 
-    st.header('Character Trivia (Non-Essential Information for Gameplay)')
-    triv_char = st.selectbox('View various trivia about a particular Genshin Impact character:', dc.index)
+    st.header('Character Trivia')
+    triv_char = st.selectbox('View various non-essential trivia about a particular Genshin Impact character:', dc.index)
     st.markdown('### Trivia Table')
     
     triv_stats = dc.loc[triv_char, ['model', 'constellation', 'birthday', 'special_dish', 'voice_eng', 
@@ -384,4 +455,3 @@ def trivia_stats():
     
 if __name__ == "__main__":
     main()
-    
